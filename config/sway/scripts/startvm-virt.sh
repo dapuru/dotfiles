f3997a5ca12e7daecb5c8487435e622cb6729aae
Daniel <67063706+dapuru@users.noreply.github.com>
2024-02-24 13:05:00 +0100

    Add files via upload

--- config/sway/scripts/startvm-virt.sh
+++ config/sway/scripts/startvm-virt.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# start vm and connect to it in fullscreen
+# start vm and connect to it - unfortunally -f for fullscreen doesn't work
 virsh start "$1"
-virt-viewer -w -a "$1"
+virt-viewer -w -a --hotkeys=toggle-fullscreen=shift+f,release-cursor=shift+r "$1"
